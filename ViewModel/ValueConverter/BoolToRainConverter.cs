847b588068e93eda2769a89e7ecdc21a7b55e140
henimex <49072861+henimex@users.noreply.github.com>
2021-03-02 23:54:54 +0300

    Converter Interface

---
+++ ViewModel/ValueConverter/BoolToRainConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WheatherApiApp.ViewModel.ValueConverter
+{
+    public class BoolToRainConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isRaining = (bool) value;
+            if (isRaining) return "Yağmur Yağıyor";
+            return "Yağmur Yağışı Yok";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
